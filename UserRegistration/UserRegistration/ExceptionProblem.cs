2b1f4fe98be19db1c35a45e53ccc2103c9fa8ea8
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-10 11:28:03 +0530

    UC3 -Add Valide Email ID Using Exception

--- UserRegistration/UserRegistration/ExceptionProblem.cs
+++ UserRegistration/UserRegistration/ExceptionProblem.cs
@@ -45,6 +45,23 @@ namespace UserRegistration
                 Console.WriteLine(e.Message);
             }
         }
+        //For email
+        public void validateEmailId(string emailID)
+
+        {
+            try
+            {
+                string stringForEmailId = "^[a-z0-9][-a-z0-9._]+@([-a-z0-9]+.)+[a-z]{2,5}$";
+                if (Regex.IsMatch(emailID, stringForEmailId))
+                    Console.WriteLine(emailID + " is Valid EmailId");
+                else
+                    Console.WriteLine(emailID + " is Invalid EmailId");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
 
     }
 }
--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -12,9 +12,14 @@ namespace UserRegistration
         {
             ExceptionProblem validateUser = new ExceptionProblem();
             validateUser.validateFirstName("Vaibhav");
-
             Console.WriteLine("--------------------");
+
             validateUser.validateLastName("Manapure");
+            Console.WriteLine("--------------------------------------");
+
+            validateUser.validateEmailId("manapure.vaibhav542@gmail.com");
+            Console.WriteLine("--------------------------------------");
+
             Console.ReadKey();
         }
     }
