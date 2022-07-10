f05af706fbd142c28652b665f810840d22688fc1
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-10 11:11:07 +0530

    Validate first Name Using Exception

---
+++ UserRegistration/UserRegistration/ExceptionProblem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class ExceptionProblem
+    {
+        //For First NAme
+        public void validateFirstName(string firstName)
+        {
+            string stringForFirstName = "^[A-Z][a-z]{3,}?";
+            try
+            {
+
+                if (Regex.IsMatch(firstName, stringForFirstName))
+                    Console.WriteLine(firstName + " is Valid");
+
+                else
+                    Console.WriteLine(firstName + " is Invalid");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExceptionProblem validateUser = new ExceptionProblem();
+            validateUser.validateFirstName("Vaibhav");
+
+            Console.ReadKey();
+        }
+    }
+}
